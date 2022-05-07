af53248b986090a707d96b6f433b157535ccacf0
Nathan <nschpro@gmail.com>
2022-05-07 17:50:50 +0200

    First and last name generator

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 function generate(){
+
+    const firstname = ['Nathan','Emma','Theo','Guillaume','Mateo','Jean'];
+    const lastname = ['Strickland','Tate','Edwards','Gibbs','Whittle','Mills'];
+
+    const randomfirst = firstname[Math.floor(Math.random() * firstname.length)];
+    const randomlast = lastname[Math.floor(Math.random() * lastname.length)];
+
+    var pseudo = "@" + randomfirst + "." + randomlast;
+
     var followers = Math.floor(Math.random() * 999) + 1;
     var likes = Math.floor(Math.random() * 999) + 1;
 
+    document.getElementById("pseudo").textContent = pseudo;
+
     document.getElementById("numf").textContent = followers;
     document.getElementById("numl").textContent = likes;
+
+    document.getElementById("firstn").textContent = randomfirst;
+    document.getElementById("lastn").textContent = randomlast;
 }
