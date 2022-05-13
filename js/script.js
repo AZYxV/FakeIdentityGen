4ebc540e6417da5c9824238eef6640f9057d55d3
Nathan <nschpro@gmail.com>
2022-05-13 10:18:33 +0200

    Switch jquery to fetch

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 function generate(){
-    
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-
-            document.getElementById("avatar").src = data['results'][0]['picture']['large'];
-            document.getElementById("pseudo").textContent = "@" + data['results'][0]['login']['username'];
+    fetch('https://randomuser.me/api/')
+    .then((res) => res.json())
+    .then((json) => {
+    console.log(json['value']);
+    document.getElementById("avatar").src = json['results'][0]['picture']['large'];
+    document.getElementById("pseudo").textContent = "@" + json['results'][0]['login']['username'];
 
 
-            document.getElementById("firstn").textContent = data['results'][0]['name']['first'];
-            document.getElementById("lastn").textContent = data['results'][0]['name']['last'];
+    document.getElementById("firstn").textContent = json['results'][0]['name']['first'];
+    document.getElementById("lastn").textContent = json['results'][0]['name']['last'];
 
-            document.getElementById("age").textContent = data['results'][0]['dob']['age'];
-            document.getElementById("gender").textContent = data['results'][0]['gender'];
+    document.getElementById("age").textContent = json['results'][0]['dob']['age'];
+    document.getElementById("gender").textContent = json['results'][0]['gender'];
 
 
-            document.getElementById("country").textContent = data['results'][0]['location']['country'];
-            document.getElementById("city").textContent = data['results'][0]['location']['city'];
+    document.getElementById("country").textContent = json['results'][0]['location']['country'];
+    document.getElementById("city").textContent = json['results'][0]['location']['city'];
 
-            document.getElementById("email").textContent = data['results'][0]['email'];
-            document.getElementById("phone").textContent = data['results'][0]['phone'];
-        }
-      });
+    document.getElementById("email").textContent = json['results'][0]['email'];
+    document.getElementById("phone").textContent = json['results'][0]['phone'];
+    })
 }
